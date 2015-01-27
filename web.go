c3e2ba68d3b60c050bbe164579a89fd017a83449
Matt Rasmussen <mrasmus@gmail.com>
2015-01-27 01:38:11 -0800

    Added  support, gofmt'd.

--- web.go
+++ web.go
@@ -1,47 +1,50 @@
 package main
 
 import (
-"time"
-"log"
-"bufio"
-"net/http"
-ical "github.com/laurent22/ical-go"
-"fmt"
-"encoding/json"
+	"bufio"
+	"encoding/json"
+	"fmt"
+	ical "github.com/laurent22/ical-go"
+	"log"
+	"net/http"
+	"time"
 )
 
-func main () {
-http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-callback := r.FormValue("jsonp")
-calid := r.FormValue("calid")
-res, _ := http.Get("https://www.google.com/calendar/ical/" + calid + "%40group.calendar.google.com/public/basic.ics")
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		callback := r.FormValue("jsonp")
+		calid := r.FormValue("calid")
+		res, _ := http.Get("https://www.google.com/calendar/ical/" + calid + "%40group.calendar.google.com/public/basic.ics")
 
-scanner := bufio.NewScanner(res.Body)
-var lines []string
-for scanner.Scan() {
-lines = append(lines, scanner.Text())
-}
+		scanner := bufio.NewScanner(res.Body)
+		var lines []string
+		for scanner.Scan() {
+			lines = append(lines, scanner.Text())
+		}
 
-node, _, _, _ := ical.ParseCalendarNode(lines, 0)
+		node, _, _, _ := ical.ParseCalendarNode(lines, 0)
 
-var summaries []string
-now := time.Now()
+		var summaries []string
+		now := time.Now()
 
-for _, child := range node.ChildrenByName("VEVENT") {
-start := child.PropDate("DTSTART", now)
-end := child.PropDate("DTEND", now)
-if now.After(start) && now.Before(end) {
-summaries = append(summaries, child.PropString("SUMMARY",""))
-}
-}
-jsonBytes, _ := json.Marshal(summaries)
-if callback != ""{
-fmt.Fprintf(w, "%s(%s)", callback, string(jsonBytes))
-} else {
-fmt.Fprintf(w, "%s", string(jsonBytes))
-}
-})
+		for _, child := range node.ChildrenByName("VEVENT") {
+			start := child.PropDate("DTSTART", now)
+			end := child.PropDate("DTEND", now)
+			if now.After(start) && now.Before(end) {
+				summaries = append(summaries, child.PropString("SUMMARY", ""))
+			}
+		}
+		jsonBytes, _ := json.Marshal(summaries)
+		if callback != "" {
+			fmt.Fprintf(w, "%s(%s)", callback, string(jsonBytes))
+		} else {
+			fmt.Fprintf(w, "%s", string(jsonBytes))
+		}
+	})
 
-log.Fatal(http.ListenAndServe(":8080", nil))
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	if err != nil {
+		panic(err)
+	}
 
 }
